653984f278234a42f5a8cb6a0a688263bb1edd14
LtGlahn <jensen72@gmail.com>
2025-03-07 00:06:50 +0100

    Robust mot ikke-indekserte data

--- sjekkNvdbObjektIdSiffer.py
+++ sjekkNvdbObjektIdSiffer.py
@@ -14,20 +14,24 @@ if __name__ == '__main__':
     data = []
     countTotal = 0 
     for objType in dakat: 
-        sok = nvdbapiv3.nvdbFagdata( objType['id'])
+        if objType['id'] >=562: 
+            sok = nvdbapiv3.nvdbFagdata( objType['id'])
 
-        tempData = []
-        countObjType = 0
-        for etobj in sok:
-            countObjType += 1  
-            if etobj['id'] < 10000: 
-                data.append( etobj ) 
-                tempData.append( etobj )
+            tempData = []
+            countObjType = 0
+            try: 
+                for etobj in sok:
+                    countObjType += 1  
+                    if etobj['id'] < 10000: 
+                        data.append( etobj ) 
+                        tempData.append( etobj )
+            except ValueError as e: 
+                print( f"Feilmelding ved henting av type {objType['id']} {objType['navn']}: {e} ")
 
-        tekst = f"{len(tempData)} objekter av type {objType['id']} {objType['navn']} med NVDB ID < 10000 av totalt {countObjType}"
-        print( tekst )
-        with open( 'logg.log', 'a') as f:
-            f.write( f"{tekst}\n") 
+            tekst = f"{len(tempData)} objekter av type {objType['id']} {objType['navn']} med NVDB ID < 10000 av totalt {countObjType}"
+            print( tekst )
+            with open( 'logg.log', 'a') as f:
+                f.write( f"{tekst}\n") 
 
     tekst = f"{len(data)} objekter totalt i NVDB med NVDB ID < 10000 av totalt {countTotal}"
     print( tekst )

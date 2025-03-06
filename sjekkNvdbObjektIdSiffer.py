9a64dff4bc268bf892cb6ea054d35d31d85eadf1
LtGlahn <jensen72@gmail.com>
2025-03-06 20:44:18 +0100

    Leter etter NVDB ID < 10000

---
+++ sjekkNvdbObjektIdSiffer.py
@@ -0,0 +1,36 @@
+""" 
+Finner alle tre- og 4siffrende NVDB objekt ID 
+"""
+
+import requests 
+import STARTHER
+import nvdbapiv3 
+import pandas as pd 
+
+
+if __name__ == '__main__': 
+
+    dakat = requests.get( 'https://nvdbapiles-v3.atlas.vegvesen.no/vegobjekttyper.json' ).json()
+    data = []
+    countTotal = 0 
+    for objType in dakat: 
+        sok = nvdbapiv3.nvdbFagdata( objType['id'])
+
+        tempData = []
+        countObjType = 0
+        for etobj in sok:
+            countObjType += 1  
+            if etobj['id'] < 10000: 
+                data.append( etobj ) 
+                tempData.append( etobj )
+
+        tekst = f"{len(tempData)} objekter av type {objType['id']} {objType['navn']} med NVDB ID < 10000 av totalt {countObjType}"
+        print( tekst )
+        with open( 'logg.log', 'a') as f:
+            f.write( f"{tekst}\n") 
+
+    tekst = f"{len(data)} objekter totalt i NVDB med NVDB ID < 10000 av totalt {countTotal}"
+    print( tekst )
+    with open( 'logg.log', 'a') as f:
+        f.write( tekst )
+

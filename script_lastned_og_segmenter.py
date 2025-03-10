42800c5360480bffbfd42730a512ecd711329f29
LtGlahn <jensen72@gmail.com>
2025-03-10 14:17:06 +0100

    Omstrukturering

--- script_lastned_og_segmenter.py
+++ script_lastned_og_segmenter.py
@@ -1,5 +1,7 @@
-""" 
-Segmenter hele nettverket til NVDB for Trondheim kommune med fartgsgrense 
+"""
+Laster ned ALLE veglenker for Trondheim kommune på vegtrasé nivå, også dem vi vanligvis ignorerer (gangfelt, gangveg, stier m.m.)
+
+Segmenterer kjørbar bilveg på fartsgrense-data 
 """
 
 import pandas as pd
@@ -25,14 +27,19 @@ if __name__ == '__main__':
     mydf = pd.DataFrame( nvdbapiv3.nvdbFagdata(105, filter={'kommune' : 5001}).to_records() )
     fart = gpd.GeoDataFrame( mydf, geometry=mydf['geometri'].apply(wkt.loads ))
 
-    # Må dele opp, segmenteringsrutinen forutsetter gyldig metrering 
+    # Må dele opp i bilveg og ikke-bilveg, segmenteringsrutinen forutsetter gyldig metrering og fartsgrense har vi jo kun på bilveg
     bilveg = heleNettverket[ heleNettverket['trafikantgruppe'] == 'K' ]
     fotveg = heleNettverket[ heleNettverket['trafikantgruppe'] != 'K' ]
 
+    # Dropper vegnettsegenskaper fartsgrensedata fordi det blir navnekollisjon og rot
+    # Typeveg henter vi fra segmentert vegnett! 
+    fart.drop( columns=['typeVeg', 'veglenkeType', 'vegkategori', 'fase', 'trafikantgruppe', 'vegnummer'], inplace=True )
+
+    # Segmenterer (left join) på bilveg og fartsgrense
     medFartsgrense = segmentering.segmenter( bilveg, fart)
 
     # Slår sammen segmentert fartsgrense med "fotveg" - datasettet 
-    heleNettverketet_medFart = pd.concat( [ medFartsgrense, fotveg ], ignore_index=True )
+    heleNettverket_medFart = pd.concat( [ medFartsgrense, fotveg ], ignore_index=True )
 
     # Fjerner overflødige kolonner
     sletteCol = ['href', 'versjon', 'Gyldig fra dato', 'segmentlengde', 'segmentretning', 'stedfesting_retning', 'stedfesting_felt',
@@ -40,10 +47,17 @@ if __name__ == '__main__':
                   'startdato', 'fase', 'nummer', 'strekning', 'delstrekning', 'fra_meter', 'til_meter',
                 'ankerpunktmeter', 'kryssdel', 'sideanleggsdel' ]
     for SLETT in sletteCol: 
-        if SLETT in heleNettverketet_medFart.columns: 
-            heleNettverketet_medFart.drop( columns=SLETT, inplace=True )
+        if SLETT in heleNettverket_medFart.columns: 
+            heleNettverket_medFart.drop( columns=SLETT, inplace=True )
+
+    for SLETT in sletteCol: 
+        if SLETT in heleNettverket.columns: 
+            heleNettverket.drop( columns=SLETT, inplace=True )
 
 
     # Lagrer resultatet 
+    # myGdf.to_file( 'trondheimNettverk.gpkg', layer='alleLenker', driver='GPKG')   # QGIS friendly
+    heleNettverket.to_file( 'trondheimNettverk.gdb', layer='alleLenker', driver='OpenFileGDB', TARGET_ARCGIS_VERSION='ARCGIS_PRO_3_2_OR_LATER')   # Esri friendly 
+
     # heleNettverket.to_file( 'trondheimNettverk.gpkg', layer='alleLenker', driver='GPKG')   # QGIS friendly
-    heleNettverketet_medFart.to_file( 'segmentert.gdb', layer='allelenker_medfart', driver='OpenFileGDB', TARGET_ARCGIS_VERSION='ARCGIS_PRO_3_2_OR_LATER')   # Esri friendly 
+    heleNettverket_medFart.to_file( 'segmentert.gdb', layer='allelenker_medfart', driver='OpenFileGDB', TARGET_ARCGIS_VERSION='ARCGIS_PRO_3_2_OR_LATER')   # Esri friendly 
